7f43cb3b9585344e6700d3d1a9d2f4f357432847
Andrew Kyllo <kyllo34@gmail.com>
2020-02-26 17:38:10 -0800

    completed lab

--- src/middleware/handleOauth.js
+++ src/middleware/handleOauth.js
@@ -1,48 +1,59 @@
 const superagent = require('superagent')
 const User = require('../models/users')
 
-const { CLIENT_ID, TOKEN_SERVER_URL, CLIENT_SECRET, API_SERVER, REMOTE_API_ENDPOINT, STATE } = process.env;
-// const TOKEN_SERVER_URL = 'https://github.com/login/oauth/access_token'
-// const CLIENT_ID = 'a5025f2d8bafd866cb1d'
-// const CLIENT_SECRET = process.env.GITHUB_APP_CLIENT_SECRET
-// const API_SERVER = 'http://localhost:3000/oauth'
-// const REMOTE_API_ENDPOINT = 'https://api.github.com/user'
+const { CLIENT_ID, TOKEN_SERVER_URL, REDIRECT_URI, CLIENT_SECRET, API_SERVER, REMOTE_API_ENDPOINT, STATE } = process.env;
 
-async function exchangeCodeForToken (code) {
-  const response = await superagent
-    .post(TOKEN_SERVER_URL)
-    .send({
-      resonse_type: 'code',
-      client_id: CLIENT_ID,
-      redirect_uri: API_SERVER,
-      client_secret: CLIENT_SECRET,
-      state: STATE,
-    })
-  return response.body.access_token
+async function exchangeCodeForToken (code, state) {
+  if (state === process.env.STATE) {
+    try {
+      const response = await superagent
+        .post(TOKEN_SERVER_URL)
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .send({
+          grant_type: 'authorization_code',
+          code: code,
+          redirect_uri: REDIRECT_URI,
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+        })
+      return response.body.access_token;
+    } catch (e) { console.error(e) }
+  }
 }
 
 async function getRemoteUsername (token) {
-  const response = await superagent
-    .get(REMOTE_API_ENDPOINT)
-    .set('Authorization', `token ${token}`)
-    .set('user-agent', 'express-app')
-  return response.body.login
+  try {
+    const response = await superagent
+      .get(REMOTE_API_ENDPOINT)
+      .set('Authorization', `Bearer ${token}`)
+    return `${response.body.localizedFirstName} ${response.body.localizedLastName}`;
+  } catch (e) { console.error(e) }
 }
 
 async function getUser (username) {
-  const user = await User.findOneAndUpdate({ username }, { upsert: true })
+  // do we already have the user created?
+  const potentialUser = await User.findOne({ username })
+  console.log(potentialUser)
+  let user;
+  if (!potentialUser) {
+    // create the user
+    const newUser = new User({ username })
+    user = await newUser.save()
+  } else {
+    user = potentialUser
+  }
   const token = user.generateToken()
   return [user, token]
 }
 
 async function handleOauth (req, res, next) {
   try {
-    const { code } = req.query
+    const { code, state } = req.query
     console.log('(1) CODE:', code)
-    const remoteToken = await exchangeCodeForToken(req.query.code)
+    const remoteToken = await exchangeCodeForToken(code, state)
     console.log('(2) ACCESS TOKEN:', remoteToken)
     const remoteUsername = await getRemoteUsername(remoteToken)
-    console.log('(3) GITHUB USER:', remoteUsername)
+    console.log('(3) LINKEDIN USER:', remoteUsername)
     const [user, token] = await getUser(remoteUsername)
     req.user = user
     req.token = token
